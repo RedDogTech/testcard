fcb8ca56a1808a5cd80c3189f78ebce8a045cb4e
Luke Moscrop <luke@moscrop.me>
2024-04-08 16:21:30 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+extern crate gstreamer as gst;
+extern crate gstreamer_video as gst_video;
+
+use crate::gst::prelude::*;
+use anyhow::Error;
+use derive_more::{Display, Error};
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "Received error from {src}: {error} (debug: {debug:?})")]
+struct ErrorMessage {
+    src: glib::GString,
+    error: glib::Error,
+    debug: Option<glib::GString>,
+}
+
+fn main() -> Result<(), Error> {
+    gst::init()?;
+
+    let pipeline = gst::Pipeline::default();
+
+    let video_caps = gst_video::VideoCapsBuilder::new()
+        .width(1920)
+        .height(1080)
+        .framerate((60, 1).into())
+        .build();
+
+    let source = gst::ElementFactory::make("videotestsrc")
+        .property_from_str("pattern", "smpte")
+        .property("is-live", true)
+        .build()?;
+
+    let overlay = gst::ElementFactory::make("textoverlay")
+        .property_from_str("text", "SDI Output 1:\n")
+        .property_from_str("halignment", "center")
+        .property_from_str("valignment", "center")
+        .property_from_str("font-desc", "Sans, 36")
+        .build()?;
+
+    let caps = gst::ElementFactory::make("capsfilter")
+        .property("caps", &video_caps)
+        .build()?;
+
+    let timecode = gst::ElementFactory::make("timecodestamper").build()?;
+    let convert = gst::ElementFactory::make("videoconvert").build()?;
+
+    let sink = gst::ElementFactory::make("decklinkvideosink")
+        .property_from_str("mode", "1080p60")
+        .property("sync", true)
+        .build()?;
+
+    pipeline.add_many([&source, &overlay, &caps, &timecode, &convert, &sink])?;
+    gst::Element::link_many([&source, &overlay, &caps, &timecode, &convert, &sink])?;
+
+    pipeline.set_state(gst::State::Playing)?;
+
+    let bus = pipeline
+        .bus()
+        .expect("Pipeline without bus. Shouldn't happen!");
+
+    for msg in bus.iter_timed(gst::ClockTime::NONE) {
+        use gst::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                pipeline.set_state(gst::State::Null)?;
+
+                return Err(ErrorMessage {
+                    src: msg
+                        .src()
+                        .map(|s| s.path_string())
+                        .unwrap_or_else(|| glib::GString::from("UNKNOWN")),
+                    error: err.error(),
+                    debug: err.debug(),
+                }
+                .into());
+            }
+            MessageView::StateChanged(s) => {
+                println!(
+                    "State changed from {:?}: {:?} -> {:?} ({:?})",
+                    s.src().map(|s| s.path_string()),
+                    s.old(),
+                    s.current(),
+                    s.pending()
+                );
+            }
+            _ => (),
+        }
+    }
+
+    pipeline.set_state(gst::State::Null)?;
+
+    Ok(())
+}
